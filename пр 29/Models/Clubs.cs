b5f021ddba606a0dc9452144c54b7a1434099f83
yankabelosludtseva <nblsldcv@gmail.com>
2026-03-15 14:28:44 +0500

    Разработанные модели. Белослудцева

---
+++ пр 29/Models/Clubs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace пр_29.Models
+{
+    public class Clubs
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string WorkTime { get; set; }
+
+    }
+}
---
+++ пр 29/Models/Users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace пр_29.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public DateTime RentStart {  get; set; }
+        public int Duration {  get; set; }
+        public int IdClub {  get; set; }
+    }
+}
